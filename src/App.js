77f14284712f13056724c06754c95a5fdba9c8d2
Kennice Ken <kennice_offor@gmail.com>
2020-05-27 18:26:18 +0800

    Added React GlobalStyle and Google fonts to our style

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import React from 'react';
+import { createGlobalStyle } from 'styled-components'
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    font-family: 'Exo', sans-serif;
+    font-weight: 300;
+  }
+  h1, h2, h3 {
+    font-family: 'Ubuntu', sans-serif;
+  }
+`;
 
 function App() {
   return (
+      <>
+    <h1>React Pizza</h1>
+    <GlobalStyle />
     <div>
-      <h1>Hello React Pizza!!</h1>
+      <h4>Welcome to React Pizza App!!</h4>
     </div>
+    </>
   );
 }
 
