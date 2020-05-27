59b763fa6706fcf5456f7a2dbd5ee10df7e12211
Kennice Ken <kennice_offor@gmail.com>
2020-05-28 07:13:52 +0800

    Added PIzzaMenu PizzaGrig and some Styling

--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -7,9 +7,9 @@ import { YummyTitle } from "../Assets/yummyTitle";
 const MenuComponentStyled = styled.div`
     background-color: ${yummiPizzacolor};
     padding: 10px;
-    margin-top: -21px;
     position: fixed;
     width: 100%;
+    z-index: 1000;
 `;
 
 const YummyLogo = styled(YummyTitle)`
